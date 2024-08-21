4626f40138b18e5927980b523fc290efc127dd50
Lucas Vilas-Bôas <lucas.oliveiravb@gmail.com>
2024-08-21 20:51:21 +0100

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -46,5 +46,5 @@ class VkImGUITemplateRecipe(ConanFile):
         cmake_layout(self)
 
     def generate(self):
-        tc = CMakeToolchain(self)
+        tc = CMakeToolchain(self, generator="Ninja")
         tc.generate()
